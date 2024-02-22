070d0cde29a53efda08af93cfb686260200060a3
Ashraful-Islam-Akash <abirashrafulislamabir@gmail.com>
2024-02-22 11:21:52 +0600

    complete problem 1 and problem 2.

---
+++ index.js
@@ -0,0 +1,11 @@
+// problem 1
+const multiply = (num1, num2, num3) => num1 * num2 * num3;
+console.log(multiply(2, 3, 4));
+
+
+// problem 2
+const mySelf = `I am a developer.
+I love to code.
+I like to eat beriyani.`;
+console.log(mySelf);
+
